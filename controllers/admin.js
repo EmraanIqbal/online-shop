4e25e668df0fa96086d56e2e2f19adaa32534386
EmraanIqbal <se.imraniqbal@gmail.com>
2023-03-25 11:01:00 +0500

    editing/deleting products

--- controllers/admin.js
+++ controllers/admin.js
@@ -11,7 +11,8 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.addProduct = (req, res, next) => {
-  const product = new Product(req?.body?.title);
+  let { title, imageUrl, price, description } = req.body;
+  const product = new Product(title, imageUrl, price, description);
 
   product.save();
   res.redirect("/");
