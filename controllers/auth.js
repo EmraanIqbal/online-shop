be9ffc4967b62e21b4be212493a11e6930c616a4
EmraanIqbal <se.imraniqbal@gmail.com>
2023-04-01 15:35:36 +0500

    adding cokie

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,14 @@
 exports.getLogin = (req, res, next) => {
+  // const isLoggedIn = req.get("Cookie").split("=")[1].trim();
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
+    isAuthenticated: false,
     //   orders: orders
   });
+};
+
+exports.postlogin = (req, res, next) => {
+  res.setHeader("Set-Cookie", "loggedIn=true");
+  res.redirect("/");
 };
