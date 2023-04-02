6687979c8663a425b1e1a6e7c3a20ad247b4a198
EmraanIqbal <se.imraniqbal@gmail.com>
2023-04-02 21:32:21 +0500

    user signup functionality implement

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcryptjs')
+const bcrypt = require("bcryptjs");
 
 const User = require("../models/user");
 
@@ -9,61 +9,84 @@ exports.getLogin = (req, res, next) => {
   //     .trim()
   //     .split('=')[1] === 'true';
   console.log(req.session.isLoggedIn);
-  res.render('auth/login', {
-    path: '/login',
-    pageTitle: 'Login',
-    isAuthenticated: false
+  res.render("auth/login", {
+    path: "/login",
+    pageTitle: "Login",
+    isAuthenticated: false,
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  user.findById('6427c08e2b1dccb541b3b6e8')
-    .then(user => {
-      req.session.isLoggedIn = true;
-      req.session.user = user;
+  const { email, password } = req.body;
+  User.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        res.redirect("/login");
+      }
+
+      bcrypt
+        .compare(password, user.password)
+        .then((doMatch) => {
+          console.log(doMatch)
+          if (doMatch) {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            return res.redirect("/");
+          }
+          res.redirect("/login");
+        })
+        .catch((err) => {
+          console.log(err);
+          res.redirect("/login");
+        });
+
       // next();
       // res.session?.save((err) => {
       //   console.log(err)
-      res.redirect('/');
+      // res.redirect('/');
       // })
-
     })
-    .catch(err => console.log(err));
-
+    .catch((err) => console.log(err));
 };
 
 exports.getSignup = (req, res, next) => {
-  res.render('auth/signup', {
-    path: '/signup',
-    pageTitle: 'Sign Up',
-    isAuthenticated: false
+  res.render("auth/signup", {
+    path: "/signup",
+    pageTitle: "Sign Up",
+    isAuthenticated: false,
   });
-}
+};
 
 exports.postSignup = (req, res, next) => {
-  const email = req.body.email
-  const password = req.body.password
-  const confirmPassword = req.body.confirmPassword
+  const email = req.body.email;
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
 
-  User.findOne({ email }).then((userDoc) => {
-    if (userDoc) {
-      return res.redirect('/signup')
-    }
-    return bcrypt.hash(password, 12).then((hashPassword) => {
-      const user = new User({
-        email, password: hashPassword, cart: { items: [] }
-      })
+  User.findOne({ email })
+    .then((userDoc) => {
+      if (userDoc) {
+        return res.redirect("/signup");
+      }
+      return bcrypt
+        .hash(password, 12)
+        .then((hashPassword) => {
+          const user = new User({
+            email,
+            password: hashPassword,
+            cart: { items: [] },
+          });
 
-      return user.save()
-    }).then(() => {
-      res.redirect('/login');
+          return user.save();
+        })
+        .then(() => {
+          res.redirect("/login");
+        });
     })
-  })
-    .catch(err => console.log(err))
+    .catch((err) => console.log(err));
 };
 
 exports.postLogout = (req, res, next) => {
   req.session.destroy(() => {
-    res.redirect('/');
-  })
+    res.redirect("/");
+  });
 };
