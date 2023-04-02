8581ba489bb501727a052713ec97f5a919ea254f
EmraanIqbal <se.imraniqbal@gmail.com>
2023-04-02 12:13:33 +0500

    login from

--- app.js
+++ app.js
@@ -14,12 +14,13 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findById("6427c08e2b1dccb541b3b6e8")
+  User.findById('6427c08e2b1dccb541b3b6e8')
     .then(user => {
       req.user = user;
       next();
@@ -29,28 +30,29 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
 mongoose
   .connect(
-    "mongodb+srv://emran:admin123@cluster0.slzge.mongodb.net/shop?retryWrites=true&w=majority"
+    'mongodb+srv://emran:admin123@cluster0.slzge.mongodb.net/shop?retryWrites=true&w=majority'
   )
-  .then((res) => {
-    console.log("Mongodb COnnected Successfully");
-    User.findOne().then((user) => {
+  .then(result => {
+    User.findOne().then(user => {
       if (!user) {
         const user = new User({
-          name: "Emraan Iqbal",
-          email: "test@test.com",
+          name: 'Max',
+          email: 'max@test.com',
           cart: {
-            items: [],
-          },
+            items: []
+          }
         });
         user.save();
       }
     });
-
     app.listen(3000);
   })
-  .catch((err) => console.log(err));
+  .catch(err => {
+    console.log(err);
+  });
